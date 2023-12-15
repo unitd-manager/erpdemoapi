686534a864b76b7a74bda7a1a52027c4ca191b08
Muthumari-1993 <119999897+Muthumari-1993@users.noreply.github.com>
2023-12-15 12:00:27 +0530

    fileupload

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -7,13 +7,13 @@ const maxFiles = 5;
 router.use(cors());
 
 router.get('/',uploadController.index);
-// router.post('/uploadFile',upload.single('file'),uploadController.uploadFile);
+router.post('/uploadFile',upload.single('file'),uploadController.uploadFile);
 router.post('/getFileList',uploadController.getFileList);
 router.get('/getFile/:name',uploadController.getFile);
 router.delete('/getFile/:name',uploadController.removeFile);
 router.post('/deleteFile',uploadController.removeFile);
 router.post('/uploadFiles',upload.array('files', maxFiles), uploadController.uploadMultiple);
-router.post('/uploadProfile',upload.array('files', maxFiles), uploadController.uploadProfile);
+// router.post('/uploadProfile',upload.array('files', maxFiles), uploadController.uploadProfile);
 router.post('/getListOfFiles',uploadController.getFilesByRecordIdAndRoomName);
 router.post('/deleteFileByRoomNameAndId',uploadController.removeFilesByRoomRecord);
 
