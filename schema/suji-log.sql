37206c0de0c278929e791884a449ebf756eb688e
Sujitha8295 <157117180+Sujitha8295@users.noreply.github.com>
2024-03-21 12:26:21 +0530

    trading

--- schema/suji-log.sql
+++ schema/suji-log.sql
@@ -1,55 +1,69 @@
 ********************************(20/03/2024 Translation(Goods Delivery-inserts field))**************************************
-INSERT INTO `translation` (`key_text`, `value`, `arb_value`, `chi_value`, `creation_date`, `modification_date`, `group_name`, `is_html_text`, `show_to_user`, `flag`) 
+INSERT INTO `translation` 
+(`translation_id`, `key_text`, `value`, `arb_value`, `chi_value`, `creation_date`, `modification_date`, `group_name`, `is_html_text`, `show_to_user`, `flag`) 
 VALUES 
-('mdTradingGoods.Delivery Code', 'Delivery Code', 'رمز التسليم', 'GoodsDelivery Delivery Code field', '', NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.Date', 'Date', 'تاريخ', 'GoodsDelivery Date field', NULL, NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.Order Code', 'Order Code', 'رمز الطلب', 'GoodsDelivery Order Code field', NULL, NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.Company Name', 'Company Name', 'اسم الشركة', 'GoodsDelivery Company Name field', NULL, NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.Department', 'Department', 'قسم', 'GoodsDelivery Department field', NULL, NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.Salesman', 'Salesman', 'بائع', 'GoodsDelivery Salesman field', NULL, NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.Reference', 'Reference', 'مرجع', 'GoodsDelivery Reference field', NULL, NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.PO Code', 'PO Code\r\n', 'رمز الشراء', 'GoodsDelivery PO Code field\r\n', NULL, NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.Status', 'Status', 'حالة', 'GoodsDelivery Status field\r\n', NULL, NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.Item Title ', 'Item Title', 'عنوان البند', 'GoodsDelivery Item Title field ', NULL, NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.Description ', 'Description', 'وصف', 'GoodsDelivery Description field ', NULL, NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.Unit ', 'Unit', 'وحدة', 'GoodsDelivery Unit field ', NULL, NULL, NULL, NULL, NULL, '0');
-('mdTradingGoods.Ordered Quantity', 'Ordered Quantity', 'الكمية المطلوبة', 'GoodsDelivery Ordered Quantity field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.Delivered Quantity', 'Delivered Quantity', 'كمية تسليمها', 'GoodsDelivery Delivered Quantity field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.Unit Price', 'Unit Price', 'اسعر الوحدة', 'GoodsDelivery Unit Price field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingGoods.Total Amount', 'Total Amount', 'المبلغ الإجمالي', 'GoodsDelivery Total Amount field', NULL, NULL, NULL, NULL, '0');
-
+(NULL, 'mdTradingGoods.Delivery Code', 'Delivery Code', 'رمز التسليم', 'GoodsDelivery Delivery Code field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Date', 'Date', 'تاريخ', 'GoodsDelivery Date field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Order Code', 'Order Code', 'رمز الطلب', 'GoodsDelivery Order Code field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Company Name', 'Company Name', 'اسم الشركة', 'GoodsDelivery Company Name field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Department', 'Department', 'قسم', 'GoodsDelivery Department field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Salesman', 'Salesman', 'بائع', 'GoodsDelivery Salesman field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Reference', 'Reference', 'مرجع', 'GoodsDelivery Reference field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.PO Code\r\n', 'PO Code\r\n', 'رمز الشراء', 'GoodsDelivery PO Code field\r\n', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Status\r\n', 'Status', 'حالة', 'GoodsDelivery Status field\r\n', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Item Title ', 'Item Title', 'عنوان البند', 'GoodsDelivery Item Title field ', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Description ', 'Description', 'وصف', 'GoodsDelivery Description field ', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Unit ', 'Unit', 'وحدة', 'GoodsDelivery Unit field ', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Ordered Quantity', 'Ordered Quantity', 'الكمية المطلوبة', 'GoodsDelivery Ordered Quantity field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Delivered Quantity', 'Delivered Quantity', 'كمية تسليمها', 'GoodsDelivery Delivered Quantity field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Unit Price', 'Unit Price', 'اسعر الوحدة', 'GoodsDelivery Unit Price field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingGoods.Total Amount', 'Total Amount', 'المبلغ الإجمالي', 'GoodsDelivery Total Amount field', NULL, NULL, NULL, NULL, NULL, '0');
 
 
 ********************************(20/03/2024 Translation(Sales Invoice-inserts field))**************************************
-INSERT INTO `translation` (`key_text`, `value`, `arb_value`, `chi_value`, `creation_date`, `modification_date`, `group_name`, `is_html_text`, `show_to_user`, `flag`)
-VALUES 
-('mdTradingSalesInvoice.Invoice Code', 'Invoice Code', 'رمز الفاتورة', 'GoodsDelivery Invoice Code field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Invoice Source', 'Invoice Source', 'مصدر الفاتورة', 'GoodsDelivery Invoice Source field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Sales Order', 'Sales Order', 'طلب المبيعات', 'GoodsDelivery Sales Order field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Company Name', 'Company Name', 'اسم الشركة', 'GoodsDelivery Company Name field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Invoice Date', 'Invoice Date', 'تاريخ الفاتورة', 'GoodsDelivery Invoice Date field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Invoice Code', 'Invoice Code', 'رمز الفاتورة', 'GoodsDelivery Invoice Code field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Invoice Due Date', 'Invoice Due Date', 'تاريخ استحقاق الفاتورة', 'GoodsDelivery Invoice Due Date field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Invoice Amount', 'Invoice Amount', 'قيمة الفاتورة', 'GoodsDelivery Invoice Amount field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Invoice Terms', 'Invoice Terms', 'شروط الفاتورة', 'GoodsDelivery Invoice Terms field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Title', 'Title', 'عنوان', 'GoodsDelivery Title field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Unit', 'Unit', 'وحدة', 'GoodsDelivery Unit field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Unit Price', 'Unit Price', 'سعر الوحدة', 'GoodsDelivery Unit Price field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Ordered Quantity', 'Ordered Quantity', 'الكمية المطلوبة', 'GoodsDelivery Ordered Quantity field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Invoice Quantity', 'Invoice Quantity', 'كمية الفاتورة', 'GoodsDelivery Invoice Quantity field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Total Cost', 'Total Cost', 'التكلفة الإجمالية', 'GoodsDelivery Total Cost field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesInvoice.Updated By', 'Updated By', 'تم التحديث بواسطة', 'GoodsDelivery Updated By field', NULL, NULL, NULL, NULL, '0');
 
+INSERT INTO `translation` 
+(`translation_id`, `key_text`, `value`, `arb_value`, `chi_value`, `creation_date`, `modification_date`, `group_name`, `is_html_text`, `show_to_user`, `flag`) 
+VALUES 
+(NULL, 'mdTradingSalesInvoice.Invoice Code', 'Invoice Code', 'رمز الفاتورة', 'GoodsDelivery Invoice Code field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Invoice Source', 'Invoice Source', 'مصدر الفاتورة', 'GoodsDelivery Invoice Source field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Sales Order', 'Sales Order', 'طلب المبيعات', 'GoodsDelivery Sales Order field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Company Name', 'Company Name', 'اسم الشركة', 'GoodsDelivery Company Name field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Invoice Date', 'Invoice Date', 'تاريخ الفاتورة', 'GoodsDelivery Invoice Date field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Invoice Due Date', 'Invoice Due Date', 'تاريخ استحقاق الفاتورة', 'GoodsDelivery Invoice Due Date field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Invoice Amount', 'Invoice Amount', 'قيمة الفاتورة', 'GoodsDelivery Invoice Amount field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Invoice Terms', 'Invoice Terms', 'شروط الفاتورة', 'GoodsDelivery Invoice Terms field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Title', 'Title', 'عنوان', 'GoodsDelivery Title field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Unit', 'Unit', 'وحدة', 'GoodsDelivery Unit field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Unit Price', 'Unit Price', 'سعر الوحدة', 'GoodsDelivery Unit Price field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Ordered Quantity', 'Ordered Quantity', 'الكمية المطلوبة', 'GoodsDelivery Ordered Quantity field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Invoice Quantity', 'Invoice Quantity', 'كمية الفاتورة', 'GoodsDelivery Invoice Quantity field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Total Cost', 'Total Cost', 'التكلفة الإجمالية', 'GoodsDelivery Total Cost field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesInvoice.Updated By', 'Updated By', 'تم التحديث بواسطة', 'GoodsDelivery Updated By field', NULL, NULL, NULL, NULL, NULL, '0');
 
 
 ********************************(20/03/2024 Translation(Sales Return-inserts field))**************************************
-INSERT INTO `translation` (`key_text`, `value`, `arb_value`, `chi_value`, `creation_date`, `modification_date`, `group_name`, `is_html_text`, `flag`)
+INSERT INTO `translation` 
+(`translation_id`, `key_text`, `value`, `arb_value`, `chi_value`, `creation_date`, `modification_date`, `group_name`, `is_html_text`, `show_to_user`, `flag`) 
 VALUES 
-('mdTradingSalesReturn.Invoice Code', 'Invoice Code', 'رمز الفاتورة', 'SalesReturn Invoice Code field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesReturn.Status', 'Status', 'حالة', 'SalesReturn Status field\r\n', NULL, NULL,  NULL, NULL, '0'),
-('mdTradingSalesReturn.Date', 'Date', 'تاريخ', 'SalesReturn Date field', NULL, NULL, NULL, NULL,  '0'),
-('mdTradingSalesReturn.Item', 'Item', 'غرض ', 'SalesReturn Item field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesReturn.Quantity', 'Quantity', 'كمية', 'SalesReturn Quantity field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesReturn.Unit Price', 'Unit Price', 'سعر الوحدة', 'SalesReturn Unit Price field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesReturn.Total', 'Total', 'المجموع ', 'SalesReturn Total field', NULL, NULL, NULL, NULL, '0'),
-('mdTradingSalesReturn.Qty Returned', 'Qty Returned', 'الكمية التي تم إرجاعها ', 'SalesReturn Qty Returned field', NULL, NULL, NULL, NULL, '0');
+(NULL, 'mdTradingSalesReturn.Invoice Code', 'Invoice Code', 'رمز الفاتورة', 'SalesReturn Invoice Code field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesReturn.Status', 'Status', 'حالة', 'SalesReturn Status field\r\n', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesReturn.Date', 'Date', 'تاريخ', 'SalesReturn Date field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesReturn.Item', 'Item', 'غرض ', 'SalesReturn Item field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesReturn.Quantity', 'Quantity', 'كمية', 'SalesReturn Quantity field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesReturn.Unit Price', 'Unit Price', 'سعر الوحدة', 'SalesReturn Unit Price field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesReturn.Total', 'Total', 'المجموع ', 'SalesReturn Total field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesReturn.Qty Returned', 'Qty Returned', 'الكمية التي تم إرجاعها ', 'SalesReturn Qty Returned field', NULL, NULL, NULL, NULL, NULL, '0');
+
+
+********************************(21/03/2024 Translation(Sales Receipt-inserts field))**************************************
+INSERT INTO `translation` 
+(`translation_id`, `key_text`, `value`, `arb_value`, `chi_value`, `creation_date`, `modification_date`, `group_name`, `is_html_text`, `show_to_user`, `flag`) 
+VALUES 
+(NULL, 'mdTradingSalesReceipt.Order No', 'Order No', 'رقم الطلب', 'SalesReceipt Order No field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesReceipt.Receipt Code', 'Receipt Code', 'رمز الاستلام', 'SalesReceipt Receipt Code field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesReceipt.Mode of Payment', 'Mode of Payment', 'طريقة الدفع ', 'SalesReceipt Mode of Payment field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesReceipt.Status', 'Status', 'حالة', 'SalesReceipt Status field\r\n', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesReceipt.Amount', 'Amount', 'كمية ', 'SalesReceipt Amount field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesReceipt.Date', 'Date', 'تاريخ', 'SalesReceipt Date field', NULL, NULL, NULL, NULL, NULL, '0'),
+(NULL, 'mdTradingSalesReceipt.Print', 'Print', 'مطبعة ', 'SalesReceipt Print field', NULL, NULL, NULL, NULL, NULL, '0'),
