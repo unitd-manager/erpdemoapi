959c2165ab9519b3f1bfd408f15929acb6f26273
Moin Uss <moin@usoftsolutions.com>
2024-03-23 17:44:57 +0530

    equipment issue api

--- schema/Moin-log.sql
+++ schema/Moin-log.sql
@@ -25,3 +25,27 @@ VALUES
 
 ALTER TABLE `change_request` ADD `change_request_title_arb` VARCHAR(255) NULL DEFAULT NULL;
 ALTER TABLE `change_request` ADD `description_arb` VARCHAR(255) NULL DEFAULT NULL;
+
+
+********************************(23/03/2024 Translation(project_document Insert Values))**************************************
+INSERT INTO `translation` (`key_text`, `value`, `arb_value`, `chi_value`, `creation_date`, `modification_date`, `group_name`, `is_html_text`, `show_to_user`, `flag`) 
+VALUES 
+('mdDocument.DOC Code', 'DOC Code', 'رمز الوثيقة', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.DOC Title', 'DOC Title', 'عنوان الوثيقة', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.Project Title', 'Project Title', 'عنوان المشروع', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.Project Start Date', 'Project Start Date', 'تاريخ بدء المشروع', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.Project End Date', 'Project End Date', 'تاريخ انتهاء المشروع', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.Quote Status', 'Quote Status', 'حالة الاقتباس', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.Budget', 'Budget', 'ميزانية', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.Company Name', 'Company Name', 'اسم الشركة', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.Contact Name', 'Contact Name', 'اسم جهة الاتصال', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.Website', 'Website', 'موقع إلكتروني', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.Email', 'Email', 'بريد إلكتروني', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.Address', 'Address', 'عنوان', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.Phone Number', 'Phone Number', 'رقم التليفون', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.PO Code', 'PO Code', 'رمز الشراء', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.State', 'State', 'ولاية', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.Country', 'Country', 'دولة', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+('mdDocument.Position', 'Position', 'موضع', NULL, NULL, NULL, NULL, NULL, NULL, '0');
+
+ALTER TABLE `change_request` ADD `document_title_arb` VARCHAR(255) NULL DEFAULT NULL;
