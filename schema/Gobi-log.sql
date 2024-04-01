ca20be091580bed25fdb585ce3875140bb9da9a5
GobikrishnanUnited <gobi@unitdtechnologies.com>
2024-04-01 18:04:26 +0530

    first commit

--- schema/Gobi-log.sql
+++ schema/Gobi-log.sql
@@ -140,3 +140,66 @@ INSERT INTO `translation` (`translation_id`, `key_text`, `value`, `arb_value`, `
 ********************************(31/03/2024 Translation(MakeSupplier field))**************************************(Gobi)
 INSERT INTO `translation` (`translation_id`, `key_text`, `value`, `arb_value`, `chi_value`, `creation_date`, `modification_date`, `group_name`, `is_html_text`, `show_to_user`, `flag`) VALUES (NULL, 'mdMakeSupplier.Date', 'Date', 'تاريخ', NULL, NULL, NULL, NULL, NULL, NULL, '0');
 INSERT INTO `translation` (`translation_id`, `key_text`, `value`, `arb_value`, `chi_value`, `creation_date`, `modification_date`, `group_name`, `is_html_text`, `show_to_user`, `flag`) VALUES (NULL, 'mdMakeSupplier.ReceiptCode', 'Receipt Code', 'رمز الاستلام', NULL, NULL, NULL, NULL, NULL, NULL, '0');
+
+
+********************************(01/04/2024 Supplier order(MakeSupplier field))**************************************(Gobi)
+ALTER TABLE `supplier_order` ADD `so_code_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `so_code`;
+ALTER TABLE `supplier_order` ADD `flag_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `flag`;
+ALTER TABLE `supplier_order` ADD `creation_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `creation_date`;
+ALTER TABLE `supplier_order` ADD `modification_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `modification_date`;
+ALTER TABLE `supplier_order` ADD `created_by_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `created_by`;
+ALTER TABLE `supplier_order` ADD `modified_by_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `modified_by`;
+ALTER TABLE `supplier_order` ADD `date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `date`;
+
+********************************(01/04/2024 Supplier order History(Supplier field))**************************************(Gobi)
+ALTER TABLE `supplier_order_history` ADD `created_by` VARCHAR(255) NULL DEFAULT NULL AFTER `modification_date`;
+ALTER TABLE `supplier_order_history` ADD `modified_by` VARCHAR(255) NULL DEFAULT NULL AFTER `created_by`;
+ALTER TABLE `supplier_order_history` ADD `creation_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `creation_date`;
+ALTER TABLE `supplier_order_history` ADD `modification_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `modification_date`;
+ALTER TABLE `supplier_order_history` ADD `created_by_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `created_by`;
+ALTER TABLE `supplier_order_history` ADD `modified_by_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `modified_by`;
+
+
+
+********************************(01/04/2024 Supplier Price list(Supplier field))**************************************(Gobi)
+ALTER TABLE `supplier_price_list` ADD `effective_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `effective_date`;
+ALTER TABLE `supplier_price_list` ADD `expiry_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `expiry_date`;
+ALTER TABLE `supplier_price_list` ADD `creation_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `creation_date`;
+ALTER TABLE `supplier_price_list` ADD `modification_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `modification_date`;
+
+********************************(01/04/2024 Supplier Price list Item(Supplier field))**************************************(Gobi)
+ALTER TABLE `supplier_price_list_item` ADD `creation_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `creation_date`;
+ALTER TABLE `supplier_price_list_item` ADD `modification_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `modification_date`;
+
+
+********************************(01/04/2024 Supplier Receipt(Supplier field))**************************************(Gobi)
+ALTER TABLE `supplier_receipt` ADD `supplier_receipt_code_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `supplier_receipt_code`;
+ALTER TABLE `supplier_receipt` ADD `date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `date`;
+ALTER TABLE `supplier_receipt` ADD `published_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `published`;
+ALTER TABLE `supplier_receipt` ADD `flag_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `flag`;
+ALTER TABLE `supplier_receipt` ADD `creation_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `creation_date`;
+ALTER TABLE `supplier_receipt` ADD `modification_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `modification_date`;
+ALTER TABLE `supplier_receipt` ADD `created_by_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `created_by`;
+ALTER TABLE `supplier_receipt` ADD `modified_by_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `modified_by`;
+ALTER TABLE `supplier_receipt` ADD `cheque_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `cheque_date`;
+ALTER TABLE `supplier_receipt` ADD `cheque_no_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `cheque_no`;
+ALTER TABLE `supplier_receipt` ADD `coi_no_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `coi_no`;
+ALTER TABLE `supplier_receipt` ADD `company_contact_salutation_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `company_contact_salutation`;
+ALTER TABLE `supplier_receipt` ADD `cust_email_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `cust_email`;
+ALTER TABLE `supplier_receipt` ADD `cust_address_po_code_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `cust_address_po_code`;
+ALTER TABLE `supplier_receipt` ADD `cust_address_country_code_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `cust_address_country_code`;
+ALTER TABLE `supplier_receipt` ADD `advance_payment_used_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `advance_payment_used`;
+
+
+********************************(01/04/2024 Supplier Receipt History(Supplier field))**************************************(Gobi)
+ALTER TABLE `supplier_receipt_history` ADD `published_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `published`;
+ALTER TABLE `supplier_receipt_history` ADD `flag_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `flag`;
+ALTER TABLE `supplier_receipt_history` ADD `creation_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `creation_date`;
+ALTER TABLE `supplier_receipt_history` ADD `modification_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `modification_date`;
+ALTER TABLE `supplier_receipt_history` ADD `created_by_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `created_by`;
+ALTER TABLE `supplier_receipt_history` ADD `modified_by_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `modified_by`;
+ALTER TABLE `supplier_receipt_history` ADD `amount_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `amount`;
+ALTER TABLE `supplier_receipt_history` ADD `purchase_order_date_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `purchase_order_date`;
+ALTER TABLE `supplier_receipt_history` ADD `invoice_paid_status_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `invoice_paid_status`;
+ALTER TABLE `supplier_receipt_history` ADD `receipt_type_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `receipt_type`;
+ALTER TABLE `supplier_receipt_history` ADD `gst_amount_arb` VARCHAR(255) NULL DEFAULT NULL AFTER `gst_amount`;
