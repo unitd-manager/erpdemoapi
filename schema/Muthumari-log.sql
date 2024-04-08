b8c509cb949c6060101118311f390ac12665e778
Muthumari-1993 <119999897+Muthumari-1993@users.noreply.github.com>
2024-04-08 18:58:31 +0530

    erpclient

--- schema/Muthumari-log.sql
+++ schema/Muthumari-log.sql
@@ -162,3 +162,158 @@ ALTER TABLE `product` ADD `product_type_arb` varchar(100) NULL DEFAULT NULL AFTE
 ALTER TABLE `product` ADD `modified_by_arb` varchar(255)  NULL DEFAULT NULL AFTER `modified_by`; 
 ALTER TABLE `product` ADD `created_by_arb` varchar(255)  NULL DEFAULT NULL AFTER `created_by`; 
 
+
+
+TRUNCATE TABLE `erpclient`.`document`; 
+TRUNCATE TABLE `erpclient`.`dormitory`; 
+TRUNCATE TABLE `erpclient`.`empleave`; 
+TRUNCATE TABLE `erpclient`.`employee`; 
+TRUNCATE TABLE `erpclient`.`employee_category`; 
+TRUNCATE TABLE `erpclient`.`employee_timesheet`; 
+TRUNCATE TABLE `erpclient`.`employee_timesheet_finance`; 
+TRUNCATE TABLE `erpclient`.`enquiry`; 
+TRUNCATE TABLE `erpclient`.`equipment_issue`; 
+TRUNCATE TABLE `erpclient`.`equipment_request`; 
+TRUNCATE TABLE `erpclient`.`equipment_request_item`; 
+TRUNCATE TABLE `erpclient`.`expense`; 
+TRUNCATE TABLE `erpclient`.`expense_group`; 
+TRUNCATE TABLE `erpclient`.`expense_sub_group`; 
+TRUNCATE TABLE `erpclient`.`geo_country`; 
+TRUNCATE TABLE `erpclient`.`goods_delivery`; 
+
+TRUNCATE TABLE `erpclient`.`goods_delivery_item`; 
+TRUNCATE TABLE `erpclient`.`goods_receipt`; 
+TRUNCATE TABLE `erpclient`.`goods_receipt_items`; 
+TRUNCATE TABLE `erpclient`.`income_group`; 
+TRUNCATE TABLE `erpclient`.`income_sub_group`; 
+TRUNCATE TABLE `erpclient`.`interest`; 
+TRUNCATE TABLE `erpclient`.`interest_contact`; 
+TRUNCATE TABLE `erpclient`.`inventory`; 
+TRUNCATE TABLE `erpclient`.`Inventory_history`; 
+TRUNCATE TABLE `erpclient`.`invoice`; 
+TRUNCATE TABLE `erpclient`.`invoice_credit_note_history`; 
+TRUNCATE TABLE `erpclient`.`invoice_item`; 
+TRUNCATE TABLE `erpclient`.`invoice_receipt_history`; 
+TRUNCATE TABLE `erpclient`.`job_information`; 
+TRUNCATE TABLE `erpclient`.`job_order`; 
+TRUNCATE TABLE `erpclient`.`journal`; 
+
+
+TRUNCATE TABLE `erpclient`.`journal_master`; 
+TRUNCATE TABLE `erpclient`.`labour_request`; 
+TRUNCATE TABLE `erpclient`.`leave_policy`; 
+TRUNCATE TABLE `erpclient`.`leave_policy_employee_type`; 
+TRUNCATE TABLE `erpclient`.`loan`; 
+TRUNCATE TABLE `erpclient`.`loan_repayment_history`; 
+TRUNCATE TABLE `erpclient`.`materials_request_line_items`; 
+TRUNCATE TABLE `erpclient`.`materials_returned`; 
+TRUNCATE TABLE `erpclient`.`material_issue`; 
+TRUNCATE TABLE `erpclient`.`material_needed`; 
+TRUNCATE TABLE `erpclient`.`material_request`; 
+TRUNCATE TABLE `erpclient`.`material_request_item`; 
+TRUNCATE TABLE `erpclient`.`media`; 
+TRUNCATE TABLE `erpclient`.`mod_acc_other_action`; 
+TRUNCATE TABLE `erpclient`.`mod_acc_room`; 
+TRUNCATE TABLE `erpclient`.`mod_acc_room_user_group`; 
+
+TRUNCATE TABLE `erpclient`.`mod_acc_user_group_other_action`; 
+TRUNCATE TABLE `erpclient`.`opportunity_costing_summary`; 
+TRUNCATE TABLE `erpclient`.`opportunity_costing_summary_history`; 
+TRUNCATE TABLE `erpclient`.`opportunity_employee`; 
+TRUNCATE TABLE `erpclient`.`opportunity_project_history`; 
+TRUNCATE TABLE `erpclient`.`opportunity_staff`; 
+TRUNCATE TABLE `erpclient`.`orders`; 
+TRUNCATE TABLE `erpclient`.`order_item`; 
+TRUNCATE TABLE `erpclient`.`other_comp_emp`; 
+TRUNCATE TABLE `erpclient`.`payment`; 
+TRUNCATE TABLE `erpclient`.`payroll_management`; 
+TRUNCATE TABLE `erpclient`.`planning_bom`; 
+TRUNCATE TABLE `erpclient`.`planning_cpanel`; 
+TRUNCATE TABLE `erpclient`.`po_product`; 
+TRUNCATE TABLE `erpclient`.`price_list`; 
+TRUNCATE TABLE `erpclient`.`price_list_item`; 
+
+TRUNCATE TABLE `erpclient`.`Print_Label`; 
+TRUNCATE TABLE `erpclient`.`productproduct_company`; 
+TRUNCATE TABLE `erpclient`.`product_pagination`; 
+TRUNCATE TABLE `erpclient`.`product_price`; 
+TRUNCATE TABLE `erpclient`.`product_price_history`; 
+TRUNCATE TABLE `erpclient`.`project`; 
+TRUNCATE TABLE `erpclient`.`project_claim`; 
+TRUNCATE TABLE `erpclient`.`project_employee`; 
+TRUNCATE TABLE `erpclient`.`project_enquiry`; 
+TRUNCATE TABLE `erpclient`.`project_job`; 
+TRUNCATE TABLE `erpclient`.`project_job_items`; 
+TRUNCATE TABLE `erpclient`.`project_materials`; 
+TRUNCATE TABLE `erpclient`.`project_planning`; 
+TRUNCATE TABLE `erpclient`.`project_quote`; 
+TRUNCATE TABLE `erpclient`.`project_quote_items`; 
+TRUNCATE TABLE `erpclient`.`project_service`; 
+
+
+TRUNCATE TABLE `erpclient`.`project_staff`; 
+TRUNCATE TABLE `erpclient`.`project_task`; 
+TRUNCATE TABLE `erpclient`.`proposal`; 
+TRUNCATE TABLE `erpclient`.`proposal_employee`; 
+TRUNCATE TABLE `erpclient`.`purchase_invoice`; 
+TRUNCATE TABLE `erpclient`.`purchase_invoice_items`; 
+TRUNCATE TABLE `erpclient`.`purchase_order`; 
+TRUNCATE TABLE `erpclient`.`purchase_quote`; 
+TRUNCATE TABLE `erpclient`.`purchase_quote_items`; 
+TRUNCATE TABLE `erpclient`.`purchase_request`; 
+TRUNCATE TABLE `erpclient`.`purchase_request_items`; 
+TRUNCATE TABLE `erpclient`.`purchase_return`; 
+TRUNCATE TABLE `erpclient`.`purchase_return_items`; 
+TRUNCATE TABLE `erpclient`.`quote`; 
+TRUNCATE TABLE `erpclient`.`quote_category`; 
+TRUNCATE TABLE `erpclient`.`quote_columns`; 
+
+
+TRUNCATE TABLE `erpclient`.`quote_items`; 
+TRUNCATE TABLE `erpclient`.`quote_items_log`; 
+TRUNCATE TABLE `erpclient`.`quote_log`; 
+TRUNCATE TABLE `erpclient`.`receipt`; 
+TRUNCATE TABLE `erpclient`.`renewals`; 
+TRUNCATE TABLE `erpclient`.`sales_return`; 
+TRUNCATE TABLE `erpclient`.`sales_return_history`; 
+TRUNCATE TABLE `erpclient`.`schedule`; 
+TRUNCATE TABLE `erpclient`.`section`; 
+TRUNCATE TABLE `erpclient`.`service`; 
+TRUNCATE TABLE `erpclient`.`setting`; 
+TRUNCATE TABLE `erpclient`.`site`; 
+TRUNCATE TABLE `erpclient`.`smart_attendance`; 
+TRUNCATE TABLE `erpclient`.`stock_history`; 
+TRUNCATE TABLE `erpclient`.`stock_transfer`; 
+TRUNCATE TABLE `erpclient`.`stock_transfer_history`; 
+
+TRUNCATE TABLE `erpclient`.`sub_category`; 
+TRUNCATE TABLE `erpclient`.`sub_con`; 
+TRUNCATE TABLE `erpclient`.`sub_con_payments`; 
+TRUNCATE TABLE `erpclient`.`sub_con_payments_history`; 
+TRUNCATE TABLE `erpclient`.`sub_con_work_order`; 
+TRUNCATE TABLE `erpclient`.`supplier`; 
+TRUNCATE TABLE `erpclient`.`supplier_order`; 
+TRUNCATE TABLE `erpclient`.`supplier_order_history`; 
+TRUNCATE TABLE `erpclient`.`supplier_price_list`; 
+TRUNCATE TABLE `erpclient`.`supplier_price_list_item`; 
+TRUNCATE TABLE `erpclient`.`supplier_receipt`; 
+TRUNCATE TABLE `erpclient`.`supplier_receipt_history`; 
+TRUNCATE TABLE `erpclient`.`support`; 
+TRUNCATE TABLE `erpclient`.`task`; 
+TRUNCATE TABLE `erpclient`.`task_history`; 
+TRUNCATE TABLE `erpclient`.`task_staff`; 
+
+
+TRUNCATE TABLE `erpclient`.`template`; 
+TRUNCATE TABLE `erpclient`.`third_party_cost`; 
+TRUNCATE TABLE `erpclient`.`timesheet`; 
+TRUNCATE TABLE `erpclient`.`training`; 
+TRUNCATE TABLE `erpclient`.`training_staff`; 
+TRUNCATE TABLE `erpclient`.`translation`; 
+TRUNCATE TABLE `erpclient`.`user_group`; 
+TRUNCATE TABLE `erpclient`.`valuelist`; 
+TRUNCATE TABLE `erpclient`.`vehicle`; 
+TRUNCATE TABLE `erpclient`.`vehicle_fuel`; 
+TRUNCATE TABLE `erpclient`.`vehicle_insurance`; 
+TRUNCATE TABLE `erpclient`.`vehicle_service`; 
+TRUNCATE TABLE `erpclient`.`work_order_line_items`; 
